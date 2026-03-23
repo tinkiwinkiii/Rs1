3e7b81d63b902c7084224857206d0962b2491d3d
tinkiwinkiii <klaramarkic12@gmail.com>
2026-03-23 23:58:10 +0100

    Prvi upload

---
+++ Market.Backend/Market.Application/Abstractions/IAppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Market.Domain.Entities.Sales;
+
+namespace Market.Application.Abstractions;
+
+// Application layer
+public interface IAppDbContext
+{
+    DbSet<ProductEntity> Products { get; }
+    DbSet<ProductCategoryEntity> ProductCategories { get; }
+    DbSet<MarketUserEntity> Users { get; }
+    DbSet<RefreshTokenEntity> RefreshTokens { get; }
+
+    DbSet<OrderEntity> Orders{ get; }
+    DbSet<OrderItemEntity> OrderItems { get; }
+
+    Task<int> SaveChangesAsync(CancellationToken ct);
+}
---
+++ Market.Backend/Market.Infrastructure/Database/DatabaseContext.cs
@@ -0,0 +1,21 @@
+﻿using Market.Application.Abstractions;
+using Market.Domain.Entities.Sales;
+
+namespace Market.Infrastructure.Database;
+
+public partial class DatabaseContext : DbContext, IAppDbContext
+{
+    public DbSet<ProductCategoryEntity> ProductCategories => Set<ProductCategoryEntity>();
+    public DbSet<ProductEntity> Products => Set<ProductEntity>();
+    public DbSet<MarketUserEntity> Users => Set<MarketUserEntity>();
+    public DbSet<RefreshTokenEntity> RefreshTokens => Set<RefreshTokenEntity>();
+
+    public DbSet<OrderEntity> Orders => Set<OrderEntity>();
+    public DbSet<OrderItemEntity> OrderItems => Set<OrderItemEntity>();
+
+    private readonly TimeProvider _clock;
+    public DatabaseContext(DbContextOptions<DatabaseContext> options, TimeProvider clock) : base(options)
+    {
+        _clock = clock;
+    }
+}
